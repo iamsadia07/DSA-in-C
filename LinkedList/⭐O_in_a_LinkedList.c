91eddc27873a73119d1395721ae861dcd042fd55
iamsadia07 <iamsadia077@gmail.com>
2026-07-24 23:40:26 +0530

    Refactor loop detection and node creation in linked list
    Refactor linked list loop detection code and improve node creation.

--- LinkedList/⭐O_in_a_LinkedList.c
+++ LinkedList/⭐O_in_a_LinkedList.c
@@ -3,56 +3,59 @@
            ← ← ← ← ← ← ← ← ← ← ← ←        */
 
 
-#include <stdio.h>
+##include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
+struct Node
+{
     int data;
-    struct Node* next;
+    struct Node *next;
 };
 
-int main() {
+// Create Node
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
 
-    // Create nodes
-    struct Node* n1 = (struct Node*)malloc(sizeof(struct Node));
-    struct Node* n2 = (struct Node*)malloc(sizeof(struct Node));
-    struct Node* n3 = (struct Node*)malloc(sizeof(struct Node));
-    struct Node* n4 = (struct Node*)malloc(sizeof(struct Node));
-    struct Node* n5 = (struct Node*)malloc(sizeof(struct Node));
-    struct Node* n6 = (struct Node*)malloc(sizeof(struct Node));
+int detectLoop(struct Node *head)
+{
+    struct Node *slow = head;
+    struct Node *fast = head;
 
-    // Assign data
-    n1->data = 10;
-    n2->data = 20;
-    n3->data = 30;
-    n4->data = 40;
-    n5->data = 50;
-    n6->data = 60;
-
-    // Connect normally
-    n1->next = n2;
-    n2->next = n3;
-    n3->next = n4;
-    n4->next = n5;
-    n5->next = n6;
-
-    //  loop: 60 -> 20
-    n6->next = n2;
-
-    // Floyd Cycle Detection
-    struct Node *slow = n1, *fast = n1;
-
-    while (fast != NULL && fast->next != NULL) {
+    while (fast != NULL && fast->next != NULL)
+    {
         slow = slow->next;
         fast = fast->next->next;
 
-        if (slow == fast) {
-            printf("Loop detected");
-            return 0;
+   if (slow == fast)
+        {
+            return 1; // Loop Found
         }
     }
 
-    printf("No loop");
+    return 0; // No Loop
+}
+
+int main()
+{
+    struct Node *head;
+
+    head = createNode(10);
+    head->next = createNode(20);
+    head->next->next = createNode(30);
+    head->next->next->next = createNode(40);
+    head->next->next->next->next = createNode(50);
+    head->next->next->next->next->next = head->next;
+    // 50 -> 20
+
+    if (detectLoop(head))
+        printf("Loop Found");
+    else
+        printf("Loop Not Found");
 
     return 0;
 }
